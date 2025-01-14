b39bf1debe7d61c84310e34c6060e72f0c74627e
Gorgeous T <tomisinerinle4@gmail.com>
2025-01-14 05:58:35 -0800

    Changed the Smtp stuff, now the sending image through email works again

--- my_requests.py
+++ my_requests.py
@@ -5,10 +5,13 @@ import imghdr
 
 class Connect:
     def send_email_pic(self, r_email, img_loc, img_name):
+        smtp_port = 587
+        smtp_server = "smtp.gmail.com"
         my_email = os.getenv('my_email')
+        print(my_email)
         password = os.getenv('email_password')
         receiver_email = r_email
-
+        
         msg = EmailMessage()
         msg['Subject'] = 'T-Digital-Sketch'
         msg['From'] = my_email
@@ -23,12 +26,19 @@ class Connect:
                 file_type = imghdr.what(img_file.name)
 
             msg.add_attachment(file_data, maintype='image', subtype=file_type, filename=file_name)
+            s = smtplib.SMTP(smtp_server, smtp_port)
+            s.ehlo()
+            s.starttls()
+            s.ehlo()
+            s.login(my_email, password)
+            s.sendmail(my_email, receiver_email, msg.as_string())
 
+            
+            # with smtplib.SMTP('smtp.gmail.com') as smtp:
+            #     print(smtp.user)
+            #     smtp.login(user=my_email, password=password)
+            #     smtp.send_message(msg)
 
-            with smtplib.SMTP('smtp.gmail.com') as smtp:
-                print(smtp.user)
-                smtp.login(user=my_email, password=password)
-                smtp.send_message(msg)
 
         except:
             return False
